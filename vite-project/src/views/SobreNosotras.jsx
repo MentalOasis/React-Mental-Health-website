0efadc3ed8c81502f469e64e9d39e0c20d70b12a
CaFeJaAr <camila.jara@genstudents.org>
2024-06-14 09:42:38 -0400

    sjas

--- vite-project/src/views/SobreNosotras.jsx
+++ vite-project/src/views/SobreNosotras.jsx
@@ -1,11 +1,38 @@
+import './SobreNosotras.css';
 
+const Nosotras = () => {
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="page-container">
+                    <div className="col text-center">
+                        <h1 className="display-4">Sobre Nosotras</h1>
+                    </div>
+                </div>
+            </div>
+            <p className="lead">Somos un equipo de estudiantes que participan en un proyecto para abordar
+            los desafíos de la salud mental en adolescentes. Creemos que es fundamental crear conciencia y
+            ofrecer herramientas prácticas para ayudar a los jóvenes a enfrentar estos desafíos de manera 
+            positiva.</p>
 
-const SobreNosotras = () => {
-    return(
-        <>
-        
-        </>
+            <div className="container mt-5">
+                <div className='row'>
+                    <div className='col text-center'>
+                        <h1 className='display-4'>Nuestra Misión</h1>
+                    </div>
+                </div>
+            </div>
+            <p className='lead'>Nuestra misión es educar, informar y empoderar a los adolescentes para que cuiden
+            su salud mental y busquen ayuda cuando la necesiten. Queremos desafiar estigmas y proporcionar un 
+            espacio seguro donde los adolescentes puedan encontrar apoyo y recursos.</p>
+
+            <div className='container'>
+                <div className=' row mb-5'>
+                    
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default SobreNosotras
+export default Nosotras
--- vite-project/src/views/TablaNutricion.jsx
+++ vite-project/src/views/TablaNutricion.jsx
@@ -1,10 +1,168 @@
+import React from 'react';
 
-const TablaNutricion = () => {
-    return(
-        <>
-        
-        </>
-    )
+const Consejos = () => {
+    return (
+        <div>
+            <h1 className="text-center">ALGUNOS CONSEJOS</h1>
+            <br />
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+                <div className="col">
+                    <div className="card h-100">
+                        <img src="./pic/ganjero.jpeg" className="card-img-top card-img mx-auto d-block" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">Recuerda que todos somos diferentes, por lo tanto visita a tu nutricionista para que te apoye con una pauta adaptada a tus necesidades.</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card h-100">
+                        <img src="./pic/cesta.jpeg" className="card-img-top card-img mx-auto d-block" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">Recuerda apoyar tus procesos de cambios de hábitos con un psicologo/a.</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card h-100">
+                        <img src="./pic/perrito.jpeg" className="card-img-top card-img mx-auto d-block" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">No sientas culpa por comer comida rápida de vez en cuando, también es sano y válido darse gustos.</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* -------------------------------------------------------------------------------------------------- */}
+            <h2 className="text-center">GUIA ALIMENTARIA DEL ADOLESCENTE</h2>
+            <br />
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Alimentos</th>
+                        <th>Aportan</th>
+                        <th>Cantidad</th>
+                        <th>Frecuencia</th>
+                        <th>1 Porción es igual a:</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Lácteos descremados</td>
+                        <td>Proteínas, calcio, vitaminas A y B</td>
+                        <td>3 porciones</td>
+                        <td>Diarias</td>
+                        <td>
+                            <ul>
+                                <li>-1 taza de leche descremada</li>
+                                <li>-1 yogurt descremados</li>
+                                <li>-1 rebanada de quesillo 3x3 cm</li>
+                                <li>-1 lámina de queso mantecoso</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Pescado</td>
+                        <td>Proteínas, hierro, zinc y cobre</td>
+                        <td>1 porción</td>
+                        <td>2 veces por semana</td>
+                        <td>
+                            <ul>
+                                <li>1 presa de pescado</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Legumbres</td>
+                        <td>Proteínas, hierro, zinc y cobre</td>
+                        <td>1 porción</td>
+                        <td>2 veces por semana</td>
+                        <td>
+                            <ul>
+                                <li>1 taza de legumbres</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Carnes y huevo</td>
+                        <td>Proteínas, hierro, zinc y cobre</td>
+                        <td>1 porción</td>
+                        <td>3 veces por semana</td>
+                        <td>
+                            <ul>
+                                <li>-1 presa de pollo o pavo</li>
+                                <li>-1 bistec pequeño a la plancha o asado (carne magra)</li>
+                                <li>-1 huevo</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Verduras</td>
+                        <td>Antioxidantes, fibra dietética y vitaminas A y C</td>
+                        <td>3 porciones</td>
+                        <td>Diaria</td>
+                        <td>
+                            <ul>
+                                <li>-1 plato de lechuga, repollo, coliflor, apio o brócoli</li>
+                                <li>-1 tomate tamaño regular</li>
+                                <li>-½ taza de acelgas, espinacas, zanahorias, betarragas o zapallos</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Frutas</td>
+                        <td>Antioxidantes, fibra dietética y vitaminas A y C</td>
+                        <td>2 porciones</td>
+                        <td>Diaria</td>
+                        <td>
+                            <ul>
+                                <li>-1 manzana, naranja, pera o membrillo</li>
+                                <li>-2 duraznos, kiwis o tuna</li>
+                                <li>-2 damascos o ciruelas</li>
+                                <li>-1 rebanada de melón</li>
+                                <li>-1 taza de uvas o cerezas</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Cereales y papas</td>
+                        <td>Hidratos de carbono, fibra dietética y proteínas</td>
+                        <td>2 porciones</td>
+                        <td>Diaria</td>
+                        <td>
+                            <ul>
+                                <li>-1 taza de arroz, fideos o papas cocida</li>
+                                <li>-1 taza de choclos, habas o arvejas</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Pan, galletas de agua o soda</td>
+                        <td>Hidratos de carbono, fibra dietética y proteínas</td>
+                        <td>2 porciones</td>
+                        <td>Diaria</td>
+                        <td>
+                            <ul>
+                                <li>-½ hallulla o marraqueta</li>
+                                <li>-1 ½ rebanada de pan molde integral</li>
+                                <li>-2 rebanadas de pan de molde blanco</li>
+                                <li>-6 galletas de agua o soda</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Agua</td>
+                        <td>----------------------------------------</td>
+                        <td>1,5 - 2 litros</td>
+                        <td>Diaria</td>
+                        <td>
+                            <ul>
+                                <li>6 - 8 vasos</li>
+                            </ul>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
-export default TablaNutricion
+export default Consejos
